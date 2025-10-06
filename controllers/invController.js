0b8b4d87d2a50edaf82bd5b56d0e4ba23104f5aa
mnhthompson <mnhthompson@gmail.com>
2025-10-06 15:45:14 -0700

    Week 4/5 setup

--- controllers/invController.js
+++ controllers/invController.js
@@ -1,7 +1,9 @@
+const { request } = require("express")
 const invModel = require("../models/inventory-model")
 const utilities = require("../utilities/")
 
 const invCont = {}
+
 
 /* ***************************
  *  Build inventory by classification view
@@ -17,6 +19,20 @@ invCont.buildByClassificationId = async function (req, res, next) {
     nav,
     grid,
   })
+}
+
+invCont.buildByInventoryId = async function (req, res, next) {
+    const inventoryId = req.params.inventoryId;
+    const data = await invModel.getInventoryByInventoryId(inventoryId); 
+    const listing = await utilities.buildItemListing(data[0]);
+    let nav = await utilities.getNav();
+    const itemName = `${data[0].inv_make} ${data[0].inv_model}`;
+
+    res.render("./inventory/listing", {
+        title: itemName,
+        nav,
+        listing,
+    })
 }
 
 module.exports = invCont
